f47b6d7f86f470dae79a19ec6536af504c7100d6
cw9dd <cw9dd@virginia.edu>
2018-10-31 19:45:06 -0400

    added r file

---
+++ models/butts/IRLvREM_new.R
@@ -0,0 +1,76 @@
+# Two tales of one city: an IRL approach to REM
+# Peter Wu
+#install.packages("informR")
+library(statnet)
+library(informR)
+library(relevent)
+library(dplyr)
+
+setwd("C:/Users/Peter/Downloads/PROJECT_IRLvREM_2018/try_butts_newtake")
+load("../relevent_sunbelt_2014.Rdata") 
+
+head(WTCPoliceCalls)
+dim(WTCPoliceCalls)
+
+max(WTCPoliceCalls$source)
+max(WTCPoliceCalls$recipient) # 37 nodes in total
+nrow(WTCPoliceCalls) # 481 events recorded
+
+# sample a state 
+max.length <- 40
+state.length <- sample(1:max.length, 1)
+state.sampled <- sample(1:37, 2)
+for (i in 1:(state.length-1)) {
+  state.sampled <- c(state.sampled, sample(1:37,2))  
+}
+
+# sample an action 
+action.sampled <- sample(1:37, 2)
+  
+# transition_function(state, action)
+transition_function <- function(s, a) {
+  if((length(s) + length(action.sampled))/2 < max.length) { #length(action.sampled) is always 2
+    s.prime <- c(s, a)
+  }else{
+    s.prime <- c(s[(length(s)-2*(max.length-1)+1):length(s)], a)
+  }
+  return(s.prime)
+}
+
+state.to <- transition_function(s = state.sampled, a = action.sampled)
+state.to
+
+# four features:
+# transitivity (PSAB-BY)
+# popularity (NTDegRec)
+# reciprocity (PSAB-BA)
+# inertia (FrPSndSnd)
+get_reward_features <- function(s) {
+  l <- length(s)
+  transitivity <- ifelse(s[l-2] == s[l-1] & s[l] != s[l-3], 1, 0)
+  popularity <- length(which(s[1:(l-2)] == s[l]))/(2*(l-2))
+  reciprocity <- ifelse(s[l-2] == s[l-1] & s[l] == s[l-3], 1, 0)
+  inertia <- ifelse(length(which(s[seq(1,(l-2),2)] == s[l-1])) > 0, 
+             length(which(s[seq(2,(l-2),2)][which(s[seq(1,(l-2),2)] == s[l-1])] == s[l]))/
+             length(which(s[seq(1,(l-2),2)] == s[l-1])),0)
+  features <- c(transitivity, popularity, reciprocity, inertia)
+  return(features)
+}
+
+get_reward_features(s = state.sampled)
+get_reward_features(s = state.to)
+
+# plug in real data
+row <- sample(5:nrow(WTCPoliceCalls), 1)
+max.length <- nrow(WTCPoliceCalls)
+
+state.from <- as.numeric(unlist(WTCPoliceCalls[1:row,c("source","recipient")]))
+#names(state.from) <- NULL
+state.from <- state.from[max(1,length(state.from)-2*(max.length-1)+1):length(state.from)]
+state.from
+
+state.to <- transition_function(state.from, a = as.numeric(WTCPoliceCalls[row+1, c("source","recipient")]))
+state.to
+
+reward <- get_reward_features(state.to)
+reward
