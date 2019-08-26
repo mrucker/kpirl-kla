440a2bc5bcd66bb4fe360b42b2111ade44c84a86
Mark Rucker <rucker.mark@gmail.com>
2019-08-26 12:20:44 -0400

    small cleanup before major policy_func refactor

--- algorithms/reinforcement learning/lspi-klspi/policy_function.m
+++ algorithms/reinforcement learning/lspi-klspi/policy_function.m
@@ -1,29 +1,24 @@
-function [action, basis] = policy_function(policy, state)
+function action = policy_function(policy, state)
 
-  actions = policy.actions(state);
+    actions = policy.actions(state);
 
-  if (rand < policy.explore)
-    action = actions(:,randi(size(actions,2)));
-    basis  = policy.basis(state, action);
-  else
-    
-    if isfield(policy, 'exemplars')
-        basis = policy.basis(state, actions);
-        phis  = policy.affin(basis, policy.exemplars);
+    if (rand < policy.explore)
+        action = actions(:,randi(size(actions,2)));
     else
-        basis = policy.basis(state, actions);
-        phis  = basis;
-    end
-    
-    q_all = phis * policy.weights;    
-    q_max = max(q_all);
-    i_max = find(q_all == q_max);
-    
-    random_index_of_max_q = i_max(randi(length(i_max)));
+        if isfield(policy, 'exemplars')
+            basis = policy.basis(state, actions);
+            phis  = policy.affin(basis, policy.exemplars);
+        else
+            basis = policy.basis(state, actions);
+            phis  = basis;
+        end
 
-    action  = actions(:,random_index_of_max_q);
-    basis   = basis(random_index_of_max_q, :);
-  end
-  
-  
-  return
+        q_all = phis * policy.weights;    
+        q_max = max(q_all);
+        i_max = find(q_all == q_max);
+
+        random_index_of_max_q = i_max(randi(length(i_max)));
+
+        action  = actions(:,random_index_of_max_q);
+    end
+end
--- algorithms/reinforcement learning/lspi-klspi/sarsa_sampler.m
+++ algorithms/reinforcement learning/lspi-klspi/sarsa_sampler.m
@@ -10,14 +10,21 @@ function sampler = sarsa_sampler(simulator, policy, max_epis, max_steps, resampl
 end
 
 function samples = samples_from_samples(samples, policy)
+    for i = 1:numel(samples)
 
+        [nextaction, nextbasis] = policy_function(policy, samples(i).nextstate);
+
+        samples(i).nextaction = nextaction;
+        samples(i).nextbasis  = nextbasis;
+        
+    end
 end
 
 function samples = samples_from_simulation(simulator, policy, n_episodes, n_steps)
 
     episode_samples = cell(1,n_episodes);
     
-    for e = 1:n_episodes
+    parfor e = 1:n_episodes
         episode_samples{e} = create_episode_samples(simulator, policy, n_steps);
     end
 
@@ -32,20 +39,23 @@ function episode_samples = create_episode_samples(simulator, policy, max_steps)
     steps  = 0;
     endsim = 0;
 
-    state           = feval(simulator);
-    [action, basis] = policy_function(policy, state);
+    [state ] = feval(simulator);
+    [action] = policy_function(policy, state);
+    [basis ]  = policy.basis(state, action);
     
     while ( (steps < max_steps) && (~endsim) )
 
         steps = steps + 1;
 
-        [nextstate, endsim    ] = feval(simulator, state, action);
-        [nextaction, nextbasis] = policy_function(policy, nextstate);
+        [nextstate, endsim] = feval(simulator, state, action);
+        [nextaction       ] = policy_function(policy, nextstate);
+        [nextbasis        ] = policy.basis(nextstate, nextaction);
+        [reward           ] = policy.reward(state);
 
         episode_samples(steps).state      = state;
         episode_samples(steps).action     = action;
         episode_samples(steps).basis      = basis;
-        episode_samples(steps).reward     = policy.reward(state);
+        episode_samples(steps).reward     = reward;
         episode_samples(steps).nextstate  = nextstate;
         episode_samples(steps).nextaction = nextaction;
         episode_samples(steps).nextbasis  = nextbasis;
