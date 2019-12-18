92bebd3c3957b78061dcad09e6b9eeec75a70892
Mark Rucker <rucker.mark@gmail.com>
2019-12-18 18:34:45 -0500

    simplifying klspi

--- algorithms/reinforcement learning/lspi-klspi/klspi.m
+++ algorithms/reinforcement learning/lspi-klspi/klspi.m
@@ -1,37 +1,10 @@
 % A wrapper that conforms KLSPI to the RL interface
 function [policy, time, policies, times] = klspi(domain, reward)
 
-    polic_func = [domain '_policy'];
-    simul_func = [domain '_simulator'];
-    param_func = [domain '_parameters'];
-    feats_func = [domain '_value_basis_klspi'];
-    
-    clear(feats_func);
-    clear(simul_func);
-    
-    [policy] = feval(polic_func);
-    [params] = feval(param_func);
-    [feats ] = feval(feats_func);
-    
-    max_iter  = params.N;
-    max_epis  = params.M;
-    max_steps = params.T;
-    epsilon   = params.epsilon;
-    resample  = params.resample;
+    params       = feval([domain '_parameters']);
+    params.basis = ald_basis(params.mu, params.kernel);
 
-    policy.reward = reward;
-    policy.feats  = feats;
+    feval([domain '_parameters'], params)
 
-    sampler  = sarsa_sampler(simul_func, policy, max_epis, max_steps, resample);
-    
-    base_alg = ald_basis(params.mu, params.kernel);
-    eval_alg = @lsq_spd;
-
-    [~, all_policies] = lspi_klspi_core(sampler, base_alg, eval_alg, policy, max_iter, epsilon);
-
-    policy = @(s) policy_function(all_policies{end}, s);
-    time   = all_policies{end}.time;
-
-    policies = cellfun(@(p) {@(s) policy_function(p, s)}, all_policies);
-    times    = cell2mat(cellfun(@(p) { p.time }, all_policies));
+    [policy, time, policies, times] = lspi(domain, reward);    
 end
