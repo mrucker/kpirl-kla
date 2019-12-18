caf20c9d0941879e5b7d18cf92bea16013a61592
Mark Rucker <rucker.mark@gmail.com>
2019-12-18 09:51:32 -0500

    simplifying lspi fetures

--- domains/huge/algos/lspi-klspi/huge_value_basis_lspi.m
+++ domains/huge/algos/lspi-klspi/huge_value_basis_lspi.m
@@ -9,12 +9,6 @@ function state2basis = huge_value_basis_lspi()
     state2basis = @state2basis_closure;
     
     function phi = state2basis_closure(state, actions)
-        if(nargin == 0)
-            phi = size(v_p,1);
-        end
-
-        if(nargin==2)
-            phi = v_p(:,v_i(t_d(state, actions)))';
-        end
+        phi = v_p(:,v_i(t_d(state, actions)))';
     end
 end
