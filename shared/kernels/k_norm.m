45ca5cf776e486d36df8960afebf36960c0dffaa
Mark Rucker <rucker.mark@gmail.com>
2019-12-18 21:48:23 -0500

    a little more readable

--- shared/kernels/k_norm.m
+++ shared/kernels/k_norm.m
@@ -3,15 +3,19 @@ function k = k_norm()
 end
 
 function n = norm(x1,x2)
+
+    x1_x2_are_equal = all(size(x1) == size(x2)) && all(all(x1 == x2));
+
+    if(x1_x2_are_equal && (size(x1,2) == 1))
+        n = 0;
+    end
     
-    if(all(size(x1) == size(x2)) && all(all(x1 == x2)))
-        if(size(x1,2) == 1)
-            n = 0;
-        else
-            n = squareform(pdist(x1'));
-        end
-    else
-            
+    if(x1_x2_are_equal && (size(x1,2) ~= 1))
+        n = squareform(pdist(x1'));
+    end
+
+    if(~x1_x2_are_equal)
         n = (dot(x1,x1,1)'+dot(x2,x2,1)-2*(x1'*x2)).^(1/2);
     end
+
 end
